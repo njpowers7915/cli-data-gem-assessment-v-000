7ea97f63933d2580da9c9ed61651d2587d8c9953
Nicholas Powers <njpowers7915@gmail.com>
2018-05-26 22:27:58 +0000

    Created list_shows and show_details methods which will return hardcoded data

--- cli-data-gem-project/resident_advisor_event_finder/lib/resident_advisor_event_finder.rb
+++
@@ -1,5 +0,0 @@
-require "resident_advisor_event_finder/version"
-
-module ResidentAdvisorEventFinder
-  # Your code goes here...
-end
--- cli-data-gem-project/resident_advisor_event_finder/lib/resident_advisor_event_finder/version.rb
+++
@@ -1,3 +0,0 @@
-module ResidentAdvisorEventFinder
-  VERSION = "0.1.0"
-end
