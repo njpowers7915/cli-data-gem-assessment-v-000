48c98c389d3b235f87b2b98a0b39a74d07706973
Nicholas Powers <njpowers7915@gmail.com>
2018-05-26 01:10:33 +0000

    Done.

---
+++ cli-data-gem-project/resident-advisor-event-finder/lib/resident/advisor/event/finder.rb
@@ -0,0 +1,11 @@
+require "resident/advisor/event/finder/version"
+
+module Resident
+  module Advisor
+    module Event
+      module Finder
+        # Your code goes here...
+      end
+    end
+  end
+end
