e5cec8c053af51e1274c74d1a4f9035ed1ae2684
Nicholas Powers <njpowers7915@gmail.com>
2018-06-13 17:31:51 +0000

    automatically backed up by learn

---
+++ cli-data-gem-project/resident-advisor-event-finder/lib/resident/advisor/event/finder/version.rb
@@ -0,0 +1,9 @@
+module Resident
+  module Advisor
+    module Event
+      module Finder
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
---
+++ cli-data-gem-project/resident-advisor-event-finder/spec/resident/advisor/event/finder_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Resident::Advisor::Event::Finder do
+  it "has a version number" do
+    expect(Resident::Advisor::Event::Finder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
